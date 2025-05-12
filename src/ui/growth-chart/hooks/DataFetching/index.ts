94e4ed082d6027b61baf97a72cdf92824f353e39
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-05-12 17:56:09 -0500

    Eliminar el hook useCustomReferences y actualizar las exportaciones en DataFetching

--- src/ui/growth-chart/hooks/DataFetching/index.ts
+++ src/ui/growth-chart/hooks/DataFetching/index.ts
@@ -1,5 +1,4 @@
 export { usePatientBirthdateAndGender } from './usePatientBirthdateAndGender';
 export { useVitalsAndBiometrics } from './useVitals';
-export { useCustomReferences } from './useCustomReferences';
 export { usePatientObservations } from './usePatientObservations';
 export { useChartDataForGender } from './useChartDataForGender';
