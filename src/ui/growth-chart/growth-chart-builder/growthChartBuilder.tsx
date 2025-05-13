2f29c8bfc79d22238eaa7b48436853a56b5c0797
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-05-12 19:32:20 -0500

    Eliminar archivos GrowthChartAnnotations y ChartTooltip para simplificar el código y mejorar la mantenibilidad

--- src/ui/growth-chart/growth-chart-builder/growthChartBuilder.tsx
+++ src/ui/growth-chart/growth-chart-builder/growthChartBuilder.tsx
@@ -2,20 +2,254 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Line } from 'react-chartjs-2';
 import type { ChartOptions } from 'chart.js/auto';
-import Chart from 'chart.js/auto';
 
 import annotationPlugin from 'chartjs-plugin-annotation';
 import AutoSizer from 'react-virtualized-auto-sizer';
 
 import { CategoryCodes, DataSetLabels, MeasurementTypeCodes } from '../config-schema';
-
+import { unitCodes, timeUnitData, TimeUnitCodes } from '../config-schema';
 import type { CategoryToLabel, ChartDataTypes } from '../config-schema';
 
-import { GrowthChartAnnotations, AnnotateLineEnd } from './growthChartAnnotations';
 import { useMeasurementPlotting } from '../hooks/useMeasurementPlotting';
 import { useChartLines } from '../utils/chartLineColorPicker';
+import type { Scriptable, ScriptableTooltipContext, TooltipPositionerMap } from 'chart.js';
+import { differenceInMonths, differenceInWeeks, differenceInYears } from 'date-fns';
 
-import { ChartTooltip } from './chartTooltip';
+import { MeasurementTypeCodesLabel } from '../config-schema';
+import { type Animation, Chart } from 'chart.js';
+
+interface TooltipConfig {
+  enabled: boolean;
+  intersect: boolean;
+  position: Scriptable<keyof TooltipPositionerMap, ScriptableTooltipContext<'line'>>;
+  backgroundColor: string;
+  bodyFont: { size: number };
+  bodyColor: string;
+  borderColor: string;
+  borderWidth: number;
+  padding: number;
+  caretPadding: number;
+  boxPadding: number;
+  usePointStyle: boolean;
+  animation: any;
+  filter: (tooltipItem: any) => boolean;
+  callbacks: {
+    title: () => string;
+    beforeLabel: (tooltipItem: any) => string;
+    label: (tooltipItem: any) => string[];
+  };
+}
+
+interface DataSet {
+  data: number[];
+  borderWidth: number;
+  borderColor: string;
+  label: string;
+}
+
+export const AnnotateLineEnd = (
+  animation: Animation & { chart?: Chart },
+  isPercentiles: boolean,
+  keysDataSet: string[],
+) => {
+  const { chart } = animation;
+  if (!chart) return;
+
+  const { ctx } = chart;
+
+  const labelsToInclude = keysDataSet;
+
+  const lines = chart.data.datasets.filter((dataset: DataSet) => labelsToInclude.includes(dataset.label));
+
+  const extractNumberFromLabel = (label: string): number => parseFloat(label.replace(/[^\d.-]/g, ''));
+
+  const adjustLabel = (labelText: number, originalLabel: string): string => {
+    if (isPercentiles) {
+      if (originalLabel === 'P01') return '0.1%';
+      if (originalLabel === 'P999') return '99.9%';
+      return `${labelText}%`;
+    }
+    if (!isPercentiles) {
+      const isNegative = originalLabel.includes('neg');
+      return isNegative ? `-${labelText}` : ` ${labelText}`;
+    }
+    return originalLabel;
+  };
+
+  const drawText = (ctx: CanvasRenderingContext2D, text: string, x: number, y: number, color: string) => {
+    ctx.fillStyle = color;
+    ctx.font = '14px Arial';
+    ctx.fillText(text, x + 3, y + 4);
+  };
+
+  lines.forEach((dataset: DataSet, index: number) => {
+    const meta = chart.getDatasetMeta(index);
+    const [lastElement] = meta.data.slice(-1);
+    const { x, y } = lastElement.getProps(['x', 'y']);
+
+    const labelText = extractNumberFromLabel(dataset.label);
+    const adjustedLabelText = adjustLabel(labelText, dataset.label);
+
+    drawText(ctx, adjustedLabelText.toString(), x, y, dataset.borderColor);
+  });
+};
+
+export interface AnnotationLabelType {
+  display: boolean;
+  content?: (value: number) => string;
+  position?: 'top' | 'bottom' | 'center' | 'start' | 'end';
+  yAdjust?: number;
+}
+
+export const GrowthChartAnnotations = (ZscoreLines: any[], datasetMetadata: any): AnnotationLabelType[] => {
+  let timeUnitConfig = {
+    singular: '',
+    plural: '',
+  };
+
+  if (
+    datasetMetadata.xAxisLabel === TimeUnitCodes.weeks ||
+    Object.values(MeasurementTypeCodesLabel).includes(datasetMetadata.xAxisLabel)
+  ) {
+    return [];
+  }
+
+  if (datasetMetadata.xAxisLabel === TimeUnitCodes.months) {
+    timeUnitConfig = timeUnitData.Years;
+  }
+
+  if (timeUnitConfig) {
+    const xValues = ZscoreLines[0]?.data.map((entry: any) => entry.x) || [];
+
+    const { divisor } = { divisor: 12 };
+
+    const annotations = xValues
+      .filter((label: number) => label % divisor === 0)
+      .map((label: number) => ({
+        display: true,
+        type: 'line',
+        scaleID: 'x',
+        borderWidth: 1.2,
+        value: label,
+        label: {
+          display: true,
+          content: () => {
+            const value = label / divisor;
+            return `${value} ${value === 1 ? timeUnitConfig.singular : timeUnitConfig.plural}`;
+          },
+          position: 'end',
+          yAdjust: 10,
+          font: [{ size: 13, weight: 'normal' }],
+          color: 'rgba(75, 75, 75)',
+          backgroundColor: 'rgba(237, 237, 237)',
+        },
+      }));
+    if ((xValues.length - 1) % 12 === 0) {
+      annotations.pop();
+    }
+    annotations.shift();
+    return annotations;
+  }
+  return [];
+};
+
+export const ChartTooltip = (
+  category: string,
+  xAxisLabel: string,
+  yAxisLabel: string,
+  dateOfBirth: Date,
+): TooltipConfig => {
+  let xUnit = '';
+  let yUnit = '';
+  const { t } = useTranslation();
+
+  if (category === CategoryCodes.hcfa_b || category === CategoryCodes.hcfa_g) {
+    yUnit = unitCodes.cm;
+  }
+
+  if (category === CategoryCodes.lhfa_b || category === CategoryCodes.lhfa_g) {
+    yUnit = unitCodes.cm;
+  }
+
+  if (category === CategoryCodes.wfa_g || category === CategoryCodes.wfa_b) {
+    yUnit = unitCodes.kg;
+  }
+
+  if (category === CategoryCodes.wflh_b || category === CategoryCodes.wflh_g) {
+    xUnit = unitCodes.cm;
+    yUnit = unitCodes.kg;
+  }
+
+  return {
+    enabled: true,
+    intersect: false,
+    position: 'nearest',
+    backgroundColor: 'white',
+    bodyFont: { size: 12 },
+    bodyColor: 'black',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 12,
+    caretPadding: 4,
+    boxPadding: 4,
+    usePointStyle: true,
+    animation: false,
+    filter: (tooltipItem) => tooltipItem.dataset.id === 'measurementData',
+    callbacks: {
+      title: () => '',
+      beforeLabel: (tooltipItem) => {
+        const date = new Date(tooltipItem.raw.eventDate).toLocaleDateString();
+        return `${t('Date')}: ${date}`;
+      },
+      label: (tooltipItem) => {
+        const date = new Date(tooltipItem.raw.eventDate);
+        let yValue = Number(tooltipItem.formattedValue.replace(',', '.'));
+        let xValue = Number(tooltipItem.label.replace(',', '.'));
+        const weeks = differenceInWeeks(date, dateOfBirth);
+
+        let xLabel = '';
+
+        yValue = Number(yValue.toFixed(2));
+        xValue = Number(xValue.toFixed(2));
+
+        const yLabel = `${yAxisLabel}: ${yValue} ${yUnit}`;
+        xLabel = `${xAxisLabel}: ${xValue} ${xUnit}`;
+
+        if (xAxisLabel === TimeUnitCodes.weeks) {
+          xLabel = `${t('Age')}: `;
+          xLabel += `${weeks} ${weeks === 1 ? timeUnitData.Weeks.singular : timeUnitData.Weeks.plural} `;
+        }
+        if (xAxisLabel === TimeUnitCodes.months) {
+          const months = differenceInMonths(date, dateOfBirth) % 12;
+          const years = differenceInYears(date, dateOfBirth);
+
+          xLabel = `${t('Age')}: `;
+
+          if (weeks <= 13) {
+            xLabel += `${weeks} ${weeks === 1 ? timeUnitData.Weeks.singular : timeUnitData.Weeks.plural} `;
+          }
+
+          if (weeks > 13) {
+            if (years > 0) {
+              xLabel += `${years} ${years === 1 ? timeUnitData.Years.singular : timeUnitData.Years.plural} `;
+            }
+
+            if (months > 0) {
+              xLabel += `${months} ${months === 1 ? timeUnitData.Months.singular : timeUnitData.Months.plural} `;
+            }
+          }
+        }
+
+        const labels = [];
+        labels.push(yLabel);
+        labels.push(xLabel);
+
+        return labels;
+      },
+    },
+  };
+};
+
 interface GrowthChartBuilderProps extends ChartDataTypes {
   category: keyof typeof CategoryToLabel;
   dataset: string;
